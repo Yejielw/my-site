c946cfd47d048f6d49217d62dec9d83bb70ee8d5
Yejiel <yejiel@Alexs-MacBook-Pro.local>
2019-06-12 11:18:00 +0300

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,16 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyDpdjp98pdHXWbmpTImF1FYUS6_OJGrRXI",
+    authDomain: "yejiel-portfolio.firebaseapp.com",
+    databaseURL: "https://yejiel-portfolio.firebaseio.com",
+    projectId: "yejiel-portfolio",
+    storageBucket: "yejiel-portfolio.appspot.com",
+    messagingSenderId: "988746688034",
+    appId: "1:988746688034:web:3eee86227e14d034"
+  }
 };
+
+
+
+
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyDpdjp98pdHXWbmpTImF1FYUS6_OJGrRXI",
+    authDomain: "yejiel-portfolio.firebaseapp.com",
+    databaseURL: "https://yejiel-portfolio.firebaseio.com",
+    projectId: "yejiel-portfolio",
+    storageBucket: "yejiel-portfolio.appspot.com",
+    messagingSenderId: "988746688034",
+    appId: "1:988746688034:web:3eee86227e14d034"
+  }
 };
 
 /*
